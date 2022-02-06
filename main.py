e5a9a865973188fc50a1a8c7e2127502a18be4b8
Christian-Kohl <christianalbert.k@gmail.com>
2022-02-06 13:32:56 +0000

    Added delete and update CRUD functionality

--- main.py
+++ main.py
@@ -43,3 +43,21 @@ def create_post(post: Post, response: Response):
     post_dict['id'] = post_id
     my_posts[post_id] = post_dict
     return {"data": post_dict}
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    try:
+        del(my_posts[id])
+    except:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} does not exist")
+    return {'message': 'post was successfully deleted'}
+
+@app.put('/posts/{id}', status_code=status.HTTP_200_OK)
+def update_posts(id: int, post: Post):
+    if my_posts[id] != None:
+        post_dict = post.dict()
+        post_dict[id] = id
+        my_posts[id] = post_dict
+    else:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="That id does not exist")
+    return post
