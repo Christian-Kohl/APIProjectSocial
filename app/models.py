adb8266e2158a73e9ca485a62b41aff535d23518
Christian-Kohl <christianalbert.k@gmail.com>
2022-02-06 19:35:53 +0000

    Further orm

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from .database import Base
-from sqlalchemy import Column, null
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import TIMESTAMP, Column, null
+from sqlalchemy import Integer, String, Boolean, text
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,4 +8,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=True)
+    published = Column(Boolean, nullable=False, server_default="TRUE")
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
