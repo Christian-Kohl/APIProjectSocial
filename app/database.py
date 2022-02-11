ac972000f20c022e42421d6648398a141d4648ab
Christian-Kohl <christianalbert.k@gmail.com>
2022-02-11 17:45:56 +0000

    Cleanup main file

--- app/database.py
+++ app/database.py
@@ -2,6 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi',
+#                                 user='postgres', password='password', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successful")
+#         break
+#     except Exception as error:
+#         print('connection failed')
+#         print(error)
+#         time.sleep(2)
+
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
